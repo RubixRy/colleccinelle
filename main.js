d1a5b560b2f55b99beb72e9b0d798cf7fc6464fb
RubixRy <ryan.chappus@gmail.com>
2024-02-16 09:11:11 -0500

    Added NASA Image of the day.

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+console.log("Hi");
 $.getJSON("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", function (data) {
-    console.log(data);
-})
+    let copyright = data.copyright;
+    let date = data.date;
+    let description = data.description;
+    let src = data.url;
+    let title = data.title;
+
+    $("#image_of_the_day_img").attr("src", src);
+    $("#image_of_the_day_link").attr("href", src);
+    $("#image_of_the_day_title").text(title);
+    $("#image_of_the_day_description").text(description);
+    $("#image_of_the_day_date").text(date);
+    $("#image_of_the_day_copyright").text(copyright);
+});
